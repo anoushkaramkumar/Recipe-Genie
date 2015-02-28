9eed9d7d9016cdd06d32c64b675c59639821465d
Anand Shetler <anandshetler@gmail.com>
2015-02-27 19:14:41 -0500

    screen 2

--- DESI KITCHEN/www/screen2.php
+++ DESI KITCHEN/www/screen2.php
@@ -1,6 +1,7 @@
 <body style="background:#F7CB71">
     <head>
     <title>DesiKitchen</title>
+    <link href="css/screen2.css" rel="stylesheet" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700,400,200,300' rel='stylesheet' type='text/css'>
     <meta charset="UTF-8">
     <script src="https://apis.google.com/js/client:platform.js" async defer></script>
@@ -17,17 +18,48 @@
                 window.history.back()
             }
         </script>
-         <img onclick="goBack()" label="New..." src= "arrow.png" style="margin: 6px; position: relative; display: block; width: 40px; z-index: 1;">
+         <img onclick="goBack()" label="New..." src= "arrow.png" style="margin: 15px 0 0 25px; position: relative; display: block; width: 10%; z-index: 1;">
     </menu>
     <a href = "screen2.php">
         <menu label="Home" style="z-index: 1; margin: 0 auto; position: relative;">
             <span class="helper"></span>
-            <img src= "home.png" style="margin: auto; position: relative; display: block; width: 40px; z-index: 1;">
+            <img src= "home.png" style="margin: 10px auto; position: relative; display: block; width: 10%; z-index: 1;">
         </menu>
     </a>
     <menu label="Help">
         <command href="help.html" label="Help" />
         <command href="about.html" label="About" />
     </menu>
-</menu>
+    <menu label="Plus" style="z-index: 1; margin: 0 auto; position: relative;">
+        <span class="helper"></span>
+        <img src= "img/plus.png" style="margin: auto; position: absolute; right: 32px; display: block; width: 10%; z-index: 1;">
+    </menu>
 </div>
+</menu>
+<table>
+  <tr>
+    <td class="fsttd">Milk</td>
+  </tr>
+  <tr>
+    <td class="sndtd">Eggs</td>
+  </tr>
+  <tr>
+    <td class="fsttd">Milk</td>
+  </tr>
+  <tr>
+    <td class="sndtd">Eggs</td>
+  </tr>
+  <tr>
+    <td class="fsttd">Milk</td>
+  </tr>
+  <tr>
+    <td class="sndtd">Eggs</td>
+  </tr>
+  <tr>
+    <td class="fsttd">Milk</td>
+  </tr>
+  <tr>
+    <td class="sndtd">Eggs</td>
+  </tr>
+</table>
+<button>
