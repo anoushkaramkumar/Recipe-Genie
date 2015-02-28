7849901534fac49b72f697d12566d1ffd9c677e5
Anoushka <ankyisthebest@gmail.com>
2015-02-28 11:49:48 -0500

    added foreign key; and inserted inventory values for each user

--- DESI KITCHEN/www/DB/Dump20150228.sql
+++ DESI KITCHEN/www/DB/Dump20150228.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `desikitchen` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `DesiKitchen`;
+USE `desikitchen`;
 -- MySQL dump 10.13  Distrib 5.6.17, for osx10.6 (i386)
 --
--- Host: localhost    Database: DesiKitchen
+-- Host: localhost    Database: desikitchen
 -- ------------------------------------------------------
--- Server version	5.5.34
+-- Server version	5.5.38
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -78,7 +78,9 @@ CREATE TABLE `Inventory` (
   `RecipeID` int(11) DEFAULT NULL,
   `ItemName` varchar(45) DEFAULT NULL,
   `ItemUser` int(11) DEFAULT NULL,
-  PRIMARY KEY (`InventoryID`)
+  PRIMARY KEY (`InventoryID`),
+  KEY `ItemUser` (`ItemUser`),
+  CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`ItemUser`) REFERENCES `User` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -88,6 +90,7 @@ CREATE TABLE `Inventory` (
 
 LOCK TABLES `Inventory` WRITE;
 /*!40000 ALTER TABLE `Inventory` DISABLE KEYS */;
+INSERT INTO `Inventory` VALUES (1,1,'Potatoes',1),(2,1,'Onions',1),(3,1,'Tomatoes',1),(4,1,'Ginger',1),(5,1,'Cucumber',1),(6,1,'Broccoli',1),(7,1,'Cauliflower',1),(8,1,'Garlic',1),(9,1,'Cheese',1),(10,1,'Bread',1),(11,1,'Oil',2),(12,1,'Sour Cream',2),(13,1,'Lettuce',2),(14,1,'Tofu',2),(15,1,'Paneer',2),(16,1,'Cream Cheese',2),(17,1,'Butter',2),(18,1,'Peas',2),(19,1,'Carrots',2),(20,1,'Green Beans',2),(21,1,'Lentils',3),(22,1,'Pickles',3),(23,1,'Bananas',3),(24,1,'Apples',3),(25,1,'Oranges',3),(26,1,'Grapes',3),(27,1,'Spinach',3),(28,1,'Saffron',3),(29,1,'Celery',3),(30,1,'Brussel Sprouts',3);
 /*!40000 ALTER TABLE `Inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,6 +168,7 @@ CREATE TABLE `User` (
 
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (1,'Anoushka Ramkumar','F','ankyisthebest@gmail.com'),(2,'Vedant Rautela','M','v.rautela01@gmail.com'),(3,'Anand Shetler','M','anandshetler@gmail.com');
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -177,4 +181,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-02-28 10:54:24
+-- Dump completed on 2015-02-28 11:48:53
